404958f6280e182600f2560ce1c4f4c060d74458
che laptop <ccald004@campus.goldsmiths.ac.uk>
2025-03-03 16:01:41 +0000

    fixing

--- UDP transport layer.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('localhost', 333))
-print("udp ready to recieve")
-while True:
-    data, client_address =  server.recvfrom(2048)
-    print(f"Received data from {client_address}: {data.decode()}")
